227987ff51737284be4f250ae838f90b2c3644c0
implicated <implicated.cc@gmail.com>
2023-07-23 18:26:23 +0800

    feat: add source code

---
+++ src/test/hashmap/HashMapDemo.java
@@ -0,0 +1,14 @@
+package test.hashmap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapDemo {
+    public static void main(String[] args) {
+        Map<String, Integer> map = new HashMap<>();
+        map.put("1", 1);
+        map.put("2", 2);
+        map.put("3", 3);
+        map.put("4", 4);
+    }
+}
